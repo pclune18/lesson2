adbe53bd5c5b68bd8f6309f00d361c5b24fbc48b
park2210 <park2210@ISS147D8-18.scdsb.pri>
2015-09-11 10:24:30 -0400

    Initial Commit

--- src/Momentum.java
+++ src/Momentum.java
@@ -4,8 +4,15 @@ public class Momentum {
 
     public static void main(String[] args) {
         Console c= new Console();
-        c.print("");
-        
+        double mass, vel, momentum;
+               
+        c.print("Enter the mass of the object: ");
+        mass=c.readDouble();
+        c.print("\nEnter the velocity of the object: ");
+        vel=c.readDouble();
+        //calc answer
+        momentum=mass*vel;
+        c.print("\n The Momentum is " + momentum);
     }
     
 }
